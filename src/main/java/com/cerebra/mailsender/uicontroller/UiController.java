080cc8645062ea3208ec5e5989faf1f2321f6a21
khalid abdellatif Khalid <khalidabdellatif187@gmail.com>
2024-05-06 22:42:41 +0200

    Rename Signin Controller To Ui Controller

--- src/main/java/com/cerebra/mailsender/uicontroller/UiController.java
+++ src/main/java/com/cerebra/mailsender/uicontroller/UiController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/ui")
 @AllArgsConstructor
-public class SignInController {
+public class UiController {
 
     private final UserService userService;
 
