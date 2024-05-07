aad53e39b6ff173b37d3a40ba887a7b90ef35979
Khalid Abd-ellatif <kabdellatif.c@saip.gov.sa>
2024-05-07 17:02:08 +0300

    Fix Update Data Type Of Click Count

--- src/main/resources/config/liquibase/changelog/update_data_type_of_column_in_mail_link_table.sql
+++ src/main/resources/config/liquibase/changelog/update_data_type_of_column_in_mail_link_table.sql
@@ -1,5 +1,5 @@
 ALTER TABLE mail.mail_link
-ALTER COLUMN click_count TYPE int4;
+ALTER COLUMN click_count TYPE int4 USING click_count::integer;
 
 ALTER TABLE mail.mail_link
 ALTER COLUMN click_count SET DEFAULT 0;
